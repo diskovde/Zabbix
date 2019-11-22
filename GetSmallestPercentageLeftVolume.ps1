a2be8c03b57996a85e461c9e2659a1ad99842738
Carl Hjerpe <github@lillecarl.com>
2019-11-22 16:08:41 +0100

    Create GetSmallestPercentageLeftVolume.ps1

---
+++ GetSmallestPercentageLeftVolume.ps1
@@ -0,0 +1,4 @@
+$($smallestpct = 1.0; foreach ($i in Get-Volume | Where-Object { $_.DriveType -eq \\\"Fixed\\\" }) {
+  $smallestpct = [math]::min($smallestpct, $i.SizeRemaining / $i.Size)
+}
+Write-Host ($smallestpct * 100))
