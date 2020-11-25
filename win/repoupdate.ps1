87b577eaa879b3f865a8560116b48a564bdde630
Carl Hjerpe <carl.hjerpe@dialect.se>
2020-11-25 16:42:09 +0100

    Restart Zabbix Agent if configuration has changed

---
+++ win/repoupdate.ps1
@@ -0,0 +1,11 @@
+﻿cd $env:ZABBIX_CONFDIR
+& git clean -f
+& git reset --hard HEAD
+$result = & git pull --force
+if ($result.Contains("Already up to date.")) {
+	Write-Host "Configuration up to date"
+}
+else {
+	Write-Host "Configuration updated, restarting Zabbix Agent"
+	Restart-Service "Zabbix Agent"
+}
