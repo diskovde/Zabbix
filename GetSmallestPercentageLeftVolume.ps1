081ccb8106157e427fa1a8be287947ff509d8a0a
Carl Hjerpe <carl.hjerpe@dialect.se>
2020-11-25 10:29:17 +0100

    remove old stuff

--- GetSmallestPercentageLeftVolume.ps1
+++
@@ -1,4 +0,0 @@
-$($smallestpct = 1.0; foreach ($i in Get-Volume | Where-Object { $_.DriveType -eq "Fixed" }) {
-  $smallestpct = [math]::min($smallestpct, $i.SizeRemaining / $i.Size)
-}
-Write-Host ($smallestpct * 100))
