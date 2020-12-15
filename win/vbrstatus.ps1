b5dc4bcdd6c379efed2ccd48f733f84d1a3eace1
Carl Hjerpe <carl.hjerpe@dialect.se>
2020-12-15 13:27:15 +0100

    Move type information
    Types aren't loaded before script finished running so we must rely on dynamic here

--- win/vbrstatus.ps1
+++ win/vbrstatus.ps1
@@ -9,7 +9,8 @@ class jobinfo
     [string]$jobname
     [string]$jobstatus
     [uint64]$lastrun
-    jobinfo([DateTime]$lastrun, [Veeam.Backup.Core.CBackupTaskSession]$veeamtasksession) {
+	# [Veeam.Backup.Core.CBackupTaskSession]$veeamtasksession
+    jobinfo([DateTime]$lastrun, $veeamtasksession) {
         $this.jobname = ($veeamtasksession.JobName + $veeamtasksession.Name).ToLower().Replace(" ", "_").Replace("å","a").Replace("ä","a").Replace("ö","o");
         $this.jobstatus = $veeamtasksession.Status
         $this.lastrun = (($lastrun | Get-Date -UFormat %s), 0 | Measure-Object -Max).Maximum
