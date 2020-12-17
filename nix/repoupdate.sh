a31b4b2b843924612a8ddec92cde9f62f0bca899
Carl Hjerpe <git@lillecarl.com>
2020-12-17 22:06:03 +0000

    Add nix repo updater

---
+++ nix/repoupdate.sh
@@ -0,0 +1,13 @@
+﻿#! /bin/pwsh
+cd $env:ZABBIX_CONFDIR
+& git clean -f | Out-Null
+& git reset --hard HEAD | Out-Null
+$result = & git pull --force
+if ($result.Contains("Already up to date.")) {
+	Write-Host "Configuration up to date"
+}
+else {
+	Write-Host "Configuration updated, restarting Zabbix Agent"
+	# Restart Zabbix Agent in a forked process
+	Start-Process -FilePath pwsh -ArgumentList '-Command "systemctl restart zabbix-agent"' | Out-Null
+}
